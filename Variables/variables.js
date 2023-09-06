f87c1e709fc108af57db5af2357a3537553ea272
Atakan Alkan <atakan.alkan29@gmail.com>
2023-09-06 17:24:38 +0300

    variables added

---
+++ Variables/variables.js
@@ -0,0 +1,22 @@
+//Variables declaration
+let car; // Just defined
+cat = "Volvo"; // Variable assignment
+console.log(car); // to see changes in console
+// let is mutable  so can be changed after defining
+
+//Variables initializng
+const password = "123oiWsad2.23";
+console.log(password);
+// const stands for constant and immutable
+
+const userName = "Atakan";
+const surName = "Alkan";
+
+console.log(userName + " " + surName); // output : Atakan Alkan
+
+// Not recommended
+var a = 5;
+var a = 6;
+console.log(a); // output : 6
+// A variable with the same name can be defined more than once.
+// This usage creates many problems
